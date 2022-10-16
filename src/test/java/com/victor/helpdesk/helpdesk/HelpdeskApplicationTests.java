055e91445653f6aae60add2a460a852afa07c733
victorneves99 <victorneves99@gmail.com>
2022-10-15 21:34:37 -0300

    projeto iniciado

---
+++ src/test/java/com/victor/helpdesk/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.helpdesk.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
