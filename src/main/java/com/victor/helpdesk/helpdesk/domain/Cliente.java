1f488267ae241fe4869dbdaa62bc7516f33f7222
victorneves99 <victorneves99@gmail.com>
2022-10-17 20:23:51 -0300

    anotações jpa para classes de dominio

--- src/main/java/com/victor/helpdesk/helpdesk/domain/Cliente.java
+++ src/main/java/com/victor/helpdesk/helpdesk/domain/Cliente.java
@@ -3,13 +3,22 @@ package com.victor.helpdesk.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.victor.helpdesk.helpdesk.domain.enums.Perfil;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class Cliente extends Pessoa {
 
+  private static final long serialVersionUID = 1L;
+
+  @OneToMany(mappedBy = "cliente")
   private List<Chamado> chamados = new ArrayList<>();
 
   public Cliente(Integer id, String nome, String cpf, String email, String senha) {
@@ -18,6 +27,7 @@ public class Cliente extends Pessoa {
 
   public Cliente() {
     super();
+    addPerfil(Perfil.CLIENTE);
   }
 
 }
