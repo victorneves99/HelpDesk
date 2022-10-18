7e0b92e60e2f6ac943d6ac5132e68e744f6f5977
victorneves99 <victorneves99@gmail.com>
2022-10-18 11:55:56 -0300

    protegendo aplicação contra serialização

--- src/main/java/com/victor/helpdesk/helpdesk/domain/Tecnico.java
+++ src/main/java/com/victor/helpdesk/helpdesk/domain/Tecnico.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.victor.helpdesk.helpdesk.domain.enums.Perfil;
 
 import lombok.Getter;
@@ -18,6 +19,7 @@ public class Tecnico extends Pessoa {
 
   private static final long serialVersionUID = 1L;
 
+  @JsonIgnore
   @OneToMany(mappedBy = "tecnico")
   private List<Chamado> chamados = new ArrayList<>();
 
