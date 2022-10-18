b799ead0f3c73ccd9427ee13fcbb60d0d95a008a
victorneves99 <victorneves99@gmail.com>
2022-10-18 09:21:31 -0300

    perfil de desenvolvimento

---
+++ src/main/java/com/victor/helpdesk/helpdesk/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.victor.helpdesk.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.victor.helpdesk.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+  @Autowired
+  private DBService dbService;
+
+  @Value("${spring.jpa.hibernate.ddl-auto}")
+  private String value;
+
+  @Bean
+  public boolean instaciaDB() {
+
+    if (value.equals("create")) {
+      this.dbService.instanciaDb();
+    }
+
+    return false;
+
+  }
+
+}
